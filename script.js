379d600e96c0c7fd0e33006529c772ed53830ac5
Mykhailo <mykhaylo@tactile.com>
2015-08-31 21:33:02 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,50 @@
+$(function(){
+    var today = new Date();
+    var months=" січня лютого березня квітня травня червня липня серпня вересня жовтня листопада грудня".split(" ");
+    var month =9;// today.getMonth() + 2;
+    var day =19;// today.getDate();
+    $(".date .month").text(months[month]);
+    $(".date .day").text(day);
+
+    $.get("days/" + month + "/" + day + ".json",function(h){
+        var imageUrl = h.imageUrl;
+        if(!imageUrl)
+        {
+            imageUrl="http://c1.staticflickr.com/9/8638/15391727113_3d5348808f_h.jpg";
+        }
+
+        $(".background").addClass("ready")
+            .css("background-image","url(" +imageUrl + ")");
+
+
+
+        for(var type in h.data)
+        {
+            var isImportant=false;
+            var text = h.data[type];
+            var el = $(".day." + type);
+            if(text)
+            {
+                if(type != 'orthodox')
+                {
+                    text = text.replace( /\./ig,"<br>");
+                }
+
+                text = text.replace( /⊕/ig,"");
+                if(type == 'ukraine' && h.isHoliday ||
+                    type == 'orthodox' && text.indexOf("⊕") != -1)
+                {
+                    isImportant=true;
+                }
+
+                el.html(text);
+            }
+
+            el.toggleClass("hidden",text == null);
+            el.toggleClass("important",isImportant);
+        }
+        $(".info.hidden").removeClass("hidden");
+    }) ;
+
+
+});
