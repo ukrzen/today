11de59c1bc0301e5559d405c284ad0dfcb55cc2f
Mykhailo <mykhaylo@tactile.com>
2015-09-09 13:14:51 +0300

    Moved all images to picasa. Fixed styles bugs. Added optimal way of image loading.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,15 @@
 $(function () {
     var today = new Date();
     var months = " січня лютого березня квітня травня червня липня серпня вересня жовтня листопада грудня".split(" ");
-    var AUTUMN_URLS = ["https://lh3.googleusercontent.com/Zl8Fo-vMumjS5hu7PSDffpZ2a3RnsBpT926pgaxBsK4=w547-h364-no",
-        "https://lh3.googleusercontent.com/x62Uwx-W9wcplARwUlS1QikPIXy4FOC1Hyt7XVoGOiU=w1984-h1322-no",
-        "https://lh3.googleusercontent.com/e0IuDAwHO4XSySwDSbAcOxyc1EIjLwVMrK-fY08laoA=w1600-h900-no",
-        "https://lh3.googleusercontent.com/WWsjn9aGJ8CnYvlfjbyhDs2J6WEAp5uwaLFloGagaHI=w1000-h727-no",
-        "https://lh3.googleusercontent.com/FpGGqH2ihrkuP7QGSnIzUh34J27INZcpUliBrvL-6Ts=w1920-h1280-no"];
-
+    var AUTUMN_URLS = ["https://lh3.googleusercontent.com/-98GKcNlTdNA/Ve9kltS7qXI/AAAAAAAAAc8/h79hBVnDN9g/s{width}-Ic42/Autumn_in_Dresden.jpg",
+        "https://lh3.googleusercontent.com/-BsY3ZsMKkJA/Ve9klmRQZtI/AAAAAAAAAc8/VDBVPPe15fY/s{width}-Ic42/15391727113_3d5348808f_h.jpg",
+        "https://lh3.googleusercontent.com/-6iuz9gxozco/Ve9klngoW6I/AAAAAAAAAc8/VxtGfBuZYjU/s{width}-Ic42/Autumn_in_Toronto.jpg",
+        "https://lh3.googleusercontent.com/-LXX6XwkxHl8/Ve9kln1OdgI/AAAAAAAAAc8/R9LwsN--84M/s{width}-Ic42/maple-leaves-background.jpg",
+        "https://lh3.googleusercontent.com/-tPP99Wyj_wE/Ve9kloYKOLI/AAAAAAAAAc8/4TCukX7hxw4/s{width}-Ic42/autumn-leaves-wallpapers-hd.jpg"];
+    var width = function(){
+       var dpi = window.devicePixelRatio ||1;
+       return parseInt(Math.round(($("body").width() / 640),0)) * 640 * dpi ;
+    };
     var addDays = function (date, num) {
         var value = date.valueOf();
         value += 86400000 * num;
@@ -20,7 +23,7 @@ $(function () {
         return number < 10? "0" + number : number;
     };
 
-
+    $("body").css("background-image","url(https://lh3.googleusercontent.com/-eVQXXrQI0hs/Ve9klglf_rI/AAAAAAAAAc8/soYpriN1exk/s{width}-Ic42/b73bcc559cb7886e72b7fd6ed101a3a71.jpg)".replace("{width}",width()));
     var delta =0;
     var showDate = function (date) {
         var month = date.getMonth() + 1;
@@ -33,7 +36,7 @@ $(function () {
             if (!imageUrl) {
                 imageUrl = random(AUTUMN_URLS);
             }
-
+            imageUrl= imageUrl.replace("{width}",width());
             $(".background").addClass("ready")
                 .css("background-image", "url(" + imageUrl + ")");
 
